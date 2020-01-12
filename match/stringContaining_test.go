08790ac0dfd70dcbce00f9a9c5ad60fb1dd3d1d6
Christopher Marfia <chris.marfia@daugherty.com>
2020-01-11 22:12:24 -0600

    add matcher to string contains

---
+++ match/stringContaining_test.go
@@ -0,0 +1,46 @@
+package match
+
+import (
+	"reflect"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("stringContaining", func() {
+	Describe("StringContaining", func() {
+		It("returns a stringContaining struct", func() {
+			actual := StringContaining("")
+
+			Expect(actual).To(BeAssignableToTypeOf(new(stringContaining)))
+		})
+	})
+
+	Describe("SupportedKinds", func() {
+		It("returns all support kinds in go", func() {
+			actual := StringContaining("").SupportedKinds()
+
+			Expect(actual).To(Equal(
+				map[reflect.Kind]struct{}{
+					reflect.String:      struct{}{},
+				}))
+		})
+	})
+
+	DescribeTable("Match returns true",
+		func(expected string, actual interface{}) {
+			Expect(StringContaining(expected).Match(actual)).To(BeTrue())
+		},
+		Entry("when the string contains the substring", "sub", "I have a substring"),
+	)
+
+	DescribeTable("Match returns false",
+		func(expected string, actual interface{}) {
+			Expect(StringContaining(expected).Match(actual)).To(BeFalse())
+		},
+		Entry("when actual is nil", "hi", nil),
+		Entry("when actual is not a string", "hi", 12),
+		Entry("when the substring does not exist in actual", "hello", "screams"),
+	)
+})
